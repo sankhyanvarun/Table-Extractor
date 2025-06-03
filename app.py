3044699814658a919bf00a201b54f0c527f6ba36
Varun Sankhyan <133485342+sankhyanvarun@users.noreply.github.com>
2025-06-03 16:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,4 +225,8 @@ if __name__ == '__main__':
     else:
         print("Warning: Poppler not found. OCR may not work properly")
     
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
